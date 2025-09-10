665a40fc2eef6e665fdef9f636a841b8185aa9b8
alihumamchohan <alihumamchohan@gmail.com>
2025-09-10 16:23:04 +0500

    app.dart

---
+++ lib/app.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:trader_gpt/src/core/routes/router.dart';
+import 'package:trader_gpt/src/shared/app_start/presentation/app_start_view.dart';
+import 'package:trader_gpt/src/shared/custom_scroll_behavour.dart';
+
+class MyApp extends ConsumerWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(context, ref) {
+    final goRouter = ref.watch(routerConfigProvider);
+
+    final buttonStyle = ButtonStyle(
+      iconColor: WidgetStateProperty.all<Color>(Colors.black),
+      foregroundColor: WidgetStateProperty.all<Color>(Colors.black),
+      padding: WidgetStateProperty.all(const EdgeInsets.all(12)),
+      shape: WidgetStateProperty.all(
+        RoundedRectangleBorder(borderRadius: BorderRadius.circular(28)),
+      ),
+    );
+
+    return GestureDetector(
+      onTap: () {
+        FocusManager.instance.primaryFocus?.unfocus();
+      },
+      child: MaterialApp.router(
+        scrollBehavior: MyCustomScrollBehavior(),
+        theme: ThemeData(
+          brightness: Brightness.dark,
+          visualDensity: VisualDensity.standard,
+          useMaterial3: true,
+          inputDecorationTheme: InputDecorationTheme(
+            border: OutlineInputBorder(borderRadius: BorderRadius.circular(8)),
+          ),
+          elevatedButtonTheme: ElevatedButtonThemeData(style: buttonStyle),
+          textButtonTheme: TextButtonThemeData(style: buttonStyle),
+          outlinedButtonTheme: OutlinedButtonThemeData(style: buttonStyle),
+        ),
+        title: 'Trader GPT',
+        debugShowCheckedModeBanner: false,
+        routerConfig: goRouter,
+        builder: (_, child) {
+          return AppStartupWidget(
+            onLoaded: (_) => MediaQuery(
+              data: MediaQuery.of(
+                context,
+              ).copyWith(textScaler: const TextScaler.linear(1.0)),
+              child: child!,
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:trader_gpt/app.dart';
 
-import 'package:trader_gpt/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
