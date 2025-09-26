615a24e2e9bd47ae93dfbbb8f4c091ac70a49e4c
Utbanchohan <utbanchohan461@gmail.com>
2025-09-26 18:04:12 +0500

    up

--- lib/src/feature/analytics/analytics.dart
+++ lib/src/feature/analytics/analytics.dart
@@ -15,7 +15,7 @@ import 'package:trader_gpt/src/shared/widgets/text_widget.dart/dm_sns_text.dart'
 class AnalyticsScreen extends StatefulWidget {
   final ChatRouting? chatRouting;
 
-   AnalyticsScreen({super.key, this.chatRouting});
+  AnalyticsScreen({super.key, this.chatRouting});
 
   @override
   State<AnalyticsScreen> createState() => _AnalyticsScreenState();
@@ -172,18 +172,18 @@ class _AnalyticsScreenState extends State<AnalyticsScreen> {
                     isScrollable: true,
                     dividerColor: Colors.transparent,
                     indicator: BoxDecoration(
-                      color:  Color(0xFF1B254B), // Selected tab background
+                      color: Color(0xFF1B254B), // Selected tab background
                       borderRadius: BorderRadius.circular(30),
                     ),
                     indicatorPadding: EdgeInsets.zero,
-                    labelPadding:  EdgeInsets.symmetric(horizontal: 8),
+                    labelPadding: EdgeInsets.symmetric(horizontal: 8),
                     labelColor: Colors.white,
-                    unselectedLabelColor:  Color(0xFFB2B2B7),
+                    unselectedLabelColor: Color(0xFFB2B2B7),
                     tabs: [
                       // ---- First Tab (with icon + text) ----
                       Tab(
                         child: Container(
-                          padding:  EdgeInsets.symmetric(
+                          padding: EdgeInsets.symmetric(
                             horizontal: 16,
                             vertical: 8,
                           ),
@@ -198,8 +198,8 @@ class _AnalyticsScreenState extends State<AnalyticsScreen> {
                                 width: 20,
                                 height: 20,
                               ),
-                               SizedBox(width: 8),
-                               Text(
+                              SizedBox(width: 8),
+                              Text(
                                 "ANALYTICS",
                                 style: TextStyle(
                                   fontSize: 14,
@@ -212,7 +212,7 @@ class _AnalyticsScreenState extends State<AnalyticsScreen> {
                       ),
 
                       // ---- Second Tab (only text) ----
-                       Tab(
+                      Tab(
                         child: Text(
                           "History",
                           style: TextStyle(
@@ -227,12 +227,12 @@ class _AnalyticsScreenState extends State<AnalyticsScreen> {
               ),
               Expanded(
                 child: TabBarView(
-              physics: NeverScrollableScrollPhysics(),
+                  physics: NeverScrollableScrollPhysics(),
 
                   children: [
                     _buildAnalyticsTab(),
 
-                     Center(
+                    Center(
                       child: Text(
                         "History Content Here",
                         style: TextStyle(color: Colors.white, fontSize: 16),
@@ -250,186 +250,196 @@ class _AnalyticsScreenState extends State<AnalyticsScreen> {
 
   /// Analytics Tab Content
   Widget _buildAnalyticsTab() {
-    return DefaultTabController(
-      length:
-          categories.length, // <- overview categories ko nested TabBar banaya
-      child: Column(
-        children: [
-          /// 🔹 SEARCH BAR
-          Padding(
-            padding: EdgeInsets.all(16),
-            child: TextFormField(
-              controller: search,
-              style:  TextStyle(color: Colors.white),
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: AppColors.fieldColor,
-                hintText: 'Search here',
-                hintStyle:  TextStyle(color: Color(0xFF8B8B97)),
-                contentPadding:  EdgeInsets.symmetric(
-                  horizontal: 20,
-                  vertical: 10,
-                ),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(50.0),
-                  borderSide: BorderSide.none,
-                ),
-                suffixIcon: InkWell(
-                  onTap: () {
-                    // debounceSearch(search.text);
-                  },
-                  child: Image.asset(Assets.images.searchNormal.path, scale: 5),
+    return Container(
+      child: DefaultTabController(
+        length: categories.length,
+        child: Column(
+          children: [
+            /// 🔹 SEARCH BAR
+            Padding(
+              padding: EdgeInsets.all(16),
+              child: TextFormField(
+                controller: search,
+                style: TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: AppColors.fieldColor,
+                  hintText: 'Search here',
+                  hintStyle: TextStyle(color: Color(0xFF8B8B97)),
+                  contentPadding: EdgeInsets.symmetric(
+                    horizontal: 20,
+                    vertical: 10,
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(50),
+                    borderSide: BorderSide.none,
+                  ),
+                  suffixIcon: InkWell(
+                    onTap: () {
+                      // debounceSearch(search.text);
+                    },
+                    child: Image.asset(
+                      Assets.images.searchNormal.path,
+                      scale: 5,
+                    ),
+                  ),
                 ),
               ),
             ),
-          ),
 
-          Container(
-            margin: EdgeInsets.only(left: 16),
-            child: TabBar(
-              physics: NeverScrollableScrollPhysics(),
-              // indicatorSize:TabBarIndicatorSize.tab,
-              isScrollable: true,
-              tabAlignment: TabAlignment.start,
-              indicator: BoxDecoration(
-                color: AppColors.color1B254B,
-                borderRadius: BorderRadius.circular(50),
-              ),
-              indicatorPadding:  EdgeInsets.symmetric(
-                horizontal: 0,
-                vertical: 4,
-              ),
-              labelColor: Colors.white,
-              labelPadding: EdgeInsets.symmetric(horizontal: 10.w),
-              unselectedLabelColor: AppColors.colorB2B2B7,
-              dividerColor: Colors.transparent,
-              tabs: List.generate(
-                categories.length,
-                (index) => Tab(
-                  child: Container(
-                    padding: EdgeInsets.symmetric(horizontal: 12, vertical: 6),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(50),
-                      border: Border.all(
-                        color: AppColors.colorB2B2B7.withOpacity(0.4),
-                        width: 1,
+            Container(
+              // color: Colors.amber,
+
+              margin: EdgeInsets.only(left: 5.w),
+              child: TabBar(
+                physics: NeverScrollableScrollPhysics(),
+                // indicatorSize:TabBarIndicatorSize.tab,
+                isScrollable: true,
+                tabAlignment: TabAlignment.start,
+                indicator: BoxDecoration(
+                  color: AppColors.color1B254B,
+                  borderRadius: BorderRadius.circular(50),
+                ),
+                indicatorPadding: EdgeInsets.symmetric(
+                  horizontal: 0,
+                  vertical: 4,
+                ),
+                labelColor: Colors.white,
+                labelPadding: EdgeInsets.symmetric(horizontal: 10.w),
+                unselectedLabelColor: AppColors.colorB2B2B7,
+                dividerColor: Colors.transparent,
+                tabs: List.generate(
+                  categories.length,
+                  (index) => Tab(
+                    child: Container(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 12,
+                        vertical: 6,
+                      ),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        border: Border.all(
+                          color: AppColors.colorB2B2B7.withOpacity(0.4),
+                          width: 1,
+                        ),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          if (categoryImages[index] != null)
+                            Image.asset(
+                              categoryImages[index]!,
+                              width: 14.w,
+                              height: 14.h,
+                            ),
+                          if (categoryImages[index] != null)
+                            SizedBox(width: 8.w),
+                          Text(
+                            categories[index],
+                            style: TextStyle(
+                              fontSize: 14.sp,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                        ],
                       ),
                     ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        if (categoryImages[index] != null)
-                          Image.asset(
-                            categoryImages[index]!,
-                            width: 14.w,
-                            height: 14.h,
-                          ),
-                        if (categoryImages[index] != null) SizedBox(width: 8.w),
-                        Text(
-                          categories[index],
-                          style: TextStyle(
-                            fontSize: 14.sp,
-                            fontWeight: FontWeight.w400,
-                          ),
-                        ),
-                      ],
-                    ),
                   ),
                 ),
               ),
             ),
-          ),
 
-          /// 🔹 Nested TabBarView
-          Expanded(
-            child: TabBarView(
-              physics: NeverScrollableScrollPhysics(),
+            /// 🔹 Nested TabBarView
+            Expanded(
+              child: TabBarView(
+                physics: NeverScrollableScrollPhysics(),
 
-              children: [
-                /// Overview Tab Content
-                _overviewContent(),
+                children: [
+                  /// Overview Tab Content
+                  _overviewContent(),
 
-                /// Company Tab Content
-                Center(
-                  child: Text(
-                    "Company Content",
-                    style: TextStyle(color: Colors.white),
+                  /// Company Tab Content
+                  Center(
+                    child: Text(
+                      "Company Content",
+                      style: TextStyle(color: Colors.white),
+                    ),
                   ),
-                ),
-                SizedBox(width: 12.w),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      children: [
-                        MdSnsText(
-                          "#${selectedStock!.symbol}",
-                          variant: TextVariant.h2,
-                          fontWeight: TextFontWeightVariant.h1,
+                  SizedBox(width: 12.w),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          MdSnsText(
+                            "#${selectedStock!.symbol}",
+                            variant: TextVariant.h2,
+                            fontWeight: TextFontWeightVariant.h1,
 
-                          color: AppColors.white,
-                        ),
-                        const SizedBox(width: 4),
-                        MdSnsText(
-                          selectedStock!.name.split("-").first.trim(),
-                          color: AppColors.colorB2B2B7,
-                          variant: TextVariant.h4,
-                          fontWeight: TextFontWeightVariant.h4,
-                        ),
-                        Icon(
-                          Icons.keyboard_arrow_down,
-                          color: AppColors.white,
-                          size: 20.sp,
-                        ),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        MdSnsText(
-                          " ${selectedStock!.changesPercentage.toStringAsFixed(2)}%",
-                          color:
-                              selectedStock!.changesPercentage
-                                  .toString()
-                                  .contains("-")
-                              ? AppColors.redFF3B3B
-                              : AppColors.white,
-                          variant: TextVariant.h4,
-                          fontWeight: TextFontWeightVariant.h4,
-                        ),
-                        const SizedBox(width: 6),
-                        Icon(
-                          selectedStock!.changesPercentage.toString().contains(
-                                "-",
-                              )
-                              ? Icons.arrow_drop_down
-                              : Icons.arrow_drop_up,
-                          color:
-                              selectedStock!.changesPercentage
-                                  .toString()
-                                  .contains("-")
-                              ? AppColors.redFF3B3B
-                              : AppColors.color00FF55,
-                          size: 20,
-                        ),
-                        MdSnsText(
-                          " ${selectedStock!.changesPercentage.toStringAsFixed(2)}%",
-                          color:
-                              selectedStock!.changesPercentage
-                                  .toString()
-                                  .contains("-")
-                              ? AppColors.redFF3B3B
-                              : AppColors.color00FF55,
-                          variant: TextVariant.h4,
-                          fontWeight: TextFontWeightVariant.h4,
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ],
+                            color: AppColors.white,
+                          ),
+                          const SizedBox(width: 4),
+                          MdSnsText(
+                            selectedStock!.name.split("-").first.trim(),
+                            color: AppColors.colorB2B2B7,
+                            variant: TextVariant.h4,
+                            fontWeight: TextFontWeightVariant.h4,
+                          ),
+                          Icon(
+                            Icons.keyboard_arrow_down,
+                            color: AppColors.white,
+                            size: 20.sp,
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          MdSnsText(
+                            " ${selectedStock!.changesPercentage.toStringAsFixed(2)}%",
+                            color:
+                                selectedStock!.changesPercentage
+                                    .toString()
+                                    .contains("-")
+                                ? AppColors.redFF3B3B
+                                : AppColors.white,
+                            variant: TextVariant.h4,
+                            fontWeight: TextFontWeightVariant.h4,
+                          ),
+                          const SizedBox(width: 6),
+                          Icon(
+                            selectedStock!.changesPercentage
+                                    .toString()
+                                    .contains("-")
+                                ? Icons.arrow_drop_down
+                                : Icons.arrow_drop_up,
+                            color:
+                                selectedStock!.changesPercentage
+                                    .toString()
+                                    .contains("-")
+                                ? AppColors.redFF3B3B
+                                : AppColors.color00FF55,
+                            size: 20,
+                          ),
+                          MdSnsText(
+                            " ${selectedStock!.changesPercentage.toStringAsFixed(2)}%",
+                            color:
+                                selectedStock!.changesPercentage
+                                    .toString()
+                                    .contains("-")
+                                ? AppColors.redFF3B3B
+                                : AppColors.color00FF55,
+                            variant: TextVariant.h4,
+                            fontWeight: TextFontWeightVariant.h4,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
@@ -438,7 +448,7 @@ class _AnalyticsScreenState extends State<AnalyticsScreen> {
   Widget _overviewContent() {
     return SafeArea(
       child: SingleChildScrollView(
-        padding:  EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+        padding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
         child: Column(
           children: [
             // Aapka poora Overview ka content (Stock Info, Charts, PerformanceTable, etc.)
