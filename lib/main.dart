0878576934d6b40b70e169eeb3d0618df7bc5e32
alihumamchohan <alihumamchohan@gmail.com>
2025-10-01 18:01:44 +0500

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,14 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
+import 'package:trader_gpt/app.dart';
+import 'package:trader_gpt/src/services/sockets/socket_service.dart';
 
-import 'app.dart';
-import 'flavors.dart';
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  setUrlStrategy(PathUrlStrategy());
+  SocketService socketService = SocketService();
+  socketService.connect();
 
-void main() {
-  F.appFlavor = Flavor.values.firstWhere(
-    (element) => element.name == appFlavor,
-  );
-
-  runApp(const App());
+  runApp(const ProviderScope(child: MyApp()));
 }
