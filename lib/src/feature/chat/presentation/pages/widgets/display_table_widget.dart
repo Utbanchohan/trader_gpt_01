b31a74d0218661e75f0bc0d3d02771687912ce09
razajaved1 <raza.javed@neksoft.com>
2025-10-28 17:28:08 +0500

    up

--- lib/src/feature/chat/presentation/pages/widgets/display_table_widget.dart
+++ lib/src/feature/chat/presentation/pages/widgets/display_table_widget.dart
@@ -180,8 +180,40 @@ class _GPTDisplayableTableContainerState
     }
   }
 
+  int _currentPage = 1;
+  final int _itemsPerPage = 10;
+
   @override
   Widget build(BuildContext context) {
+    final int _totalItems = widget.tableData.data.length;
+    final double _totalPages = widget.tableData.data.length / 10;
+
+    final double startItem = ((_currentPage - 1) * _itemsPerPage) + 1;
+    final int endItem = _currentPage * _itemsPerPage > _totalItems
+        ? _totalItems
+        : _currentPage * _itemsPerPage;
+    final String displayText = '$startItem-$endItem of $_totalItems';
+
+    // Logic for navigation
+    void goToFirstPage() => setState(() => _currentPage = 1);
+    void goToPrevPage() =>
+        setState(() => _currentPage = _currentPage > 1 ? _currentPage - 1 : 1);
+    void goToNextPage() => setState(
+      () => _currentPage = _currentPage < _totalPages
+          ? _currentPage + 1
+          : _totalPages.ceil(),
+    );
+    void goToLastPage() => setState(() => _currentPage = _totalPages.ceil());
+
+    final bool isFirstPage = _currentPage == 1;
+    final bool isLastPage = _currentPage == _totalPages;
+    final int startIndex = (_currentPage - 1) * _itemsPerPage;
+
+    final List<Map<String, dynamic>> pagedData = widget.tableData.data
+        .skip(startIndex)
+        .take(_itemsPerPage)
+        .toList();
+
     return Container(
       decoration: BoxDecoration(
         color: AppColors.color091224,
@@ -189,45 +221,170 @@ class _GPTDisplayableTableContainerState
         border: Border.all(color: AppColors.color0x0x1AB3B3B3, width: 1),
       ),
       clipBehavior: Clip.antiAlias,
-      height: 450,
+
       width: double.infinity,
-      padding: const EdgeInsets.all(8),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(20),
-        child: SingleChildScrollView(
-          child: PaginatedDataTable(
-            showFirstLastButtons: true,
-            onPageChanged: (val) {},
-            showEmptyRows: false,
-            rowsPerPage: 5,
-            headingRowHeight: 40.h,
-            columnSpacing: 20,
-            horizontalMargin: 15,
-            dataRowMaxHeight: 60.h,
-            arrowHeadColor: AppColors.white,
-            headingRowColor: WidgetStateProperty.resolveWith<Color?>((
-              Set<WidgetState> states,
-            ) {
-              return AppColors.color1B254B;
-            }),
-            columns: widget.tableData.cols
-                .map(
-                  (c) => DataColumn(
-                    label: MdSnsText(
-                      c.header,
+      child: Column(
+        children: [
+          SizedBox(
+            height: 630.h,
+            child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: DataTable(
+                headingRowHeight: 40.h,
+                columnSpacing: 20,
+                horizontalMargin: 15,
+
+                dataRowMaxHeight: 60.h,
+                headingRowColor: WidgetStateProperty.resolveWith<Color?>((
+                  Set<WidgetState> states,
+                ) {
+                  return AppColors.color1B254B;
+                }),
+                columns: widget.tableData.cols
+                    .map(
+                      (c) => DataColumn(
+                        label: MdSnsText(
+                          c.header,
+                          variant: TextVariant.h4,
+                          fontWeight: TextFontWeightVariant.h2,
+                          color: AppColors.white,
+                        ),
+                      ),
+                    )
+                    .toList(),
+
+                rows:
+                    MyDataSource(
+                      TableData(cols: widget.tableData.cols, data: pagedData),
+                    ).tableData.data.asMap().entries.map((entry) {
+                      final index = entry.key;
+                      final row = entry.value;
+                      final color = index.isEven
+                          ? WidgetStateProperty.all(Colors.transparent)
+                          : WidgetStateProperty.all(AppColors.shadowColor);
+                      return DataRow(
+                        color: color,
+                        cells: widget.tableData.cols
+                            .map(
+                              (col) => DataCell(
+                                MdSnsText(
+                                  Filters.systemNumberConvention(
+                                    row[col.key]?.toString() ?? 'N/A',
+                                    isPrice: false,
+                                    isAbs: false,
+                                  ),
+                                  variant: TextVariant.h4,
+                                  fontWeight: TextFontWeightVariant.h4,
+                                  color: AppColors.white,
+                                ),
+                              ),
+                            )
+                            .toList(),
+                      );
+                    }).toList(),
+
+                //  PaginatedDataTable(
+                //   initialFirstRowIndex: 0,
+                //   showFirstLastButtons: true,
+                //   onPageChanged: (val) {},
+                //   showEmptyRows: false,
+                //   rowsPerPage: 5,
+                //   headingRowHeight: 40.h,
+                //   columnSpacing: 20,
+                //   horizontalMargin: 15,
+                //   dataRowMaxHeight: 60.h,
+                //   availableRowsPerPage: const <int>[5, 5 * 2, 5 * 5, 5 * 10],
+                //   arrowHeadColor: AppColors.white,
+                //   headingRowColor: WidgetStateProperty.resolveWith<Color?>((
+                //     Set<WidgetState> states,
+                //   ) {
+                //     return AppColors.color1B254B;
+                //   }),
+
+                //   columns: widget.tableData.cols
+                //       .map(
+                //         (c) => DataColumn(
+                //           label: MdSnsText(
+                //             c.header,
+                //             variant: TextVariant.h4,
+                //             fontWeight: TextFontWeightVariant.h2,
+                //             color: AppColors.white,
+                //           ),
+                //         ),
+                //       )
+                //       .toList(),
+                //   source: _dataSource,
+              ),
+            ),
+          ),
+          SizedBox(height: 10),
+          Card(
+            elevation: 2,
+            child: Container(
+              width: double.infinity, // 👈 ensures full width
+              color: AppColors.color091224,
+              padding: const EdgeInsets.symmetric(
+                vertical: 8.0,
+                horizontal: 16.0,
+              ),
+              child: Align(
+                alignment:
+                    Alignment.centerRight, // 👈 moves whole row to right side
+                child: Row(
+                  mainAxisSize:
+                      MainAxisSize.min, // 👈 keeps row as compact as needed
+                  children: [
+                    // 1. The Display Text
+                    MdSnsText(
+                      displayText,
                       variant: TextVariant.h4,
-                      fontWeight: TextFontWeightVariant.h2,
+                      fontWeight: TextFontWeightVariant.h4,
                       color: AppColors.white,
                     ),
-                  ),
-                )
-                .toList(),
-            source: _dataSource,
+
+                    const SizedBox(width: 15), // Spacer
+                    // 2. Go to First Page (<<)
+                    _buildControlIcon(
+                      Icons.first_page,
+                      isFirstPage ? null : goToFirstPage,
+                    ),
+
+                    // 3. Go to Previous Page (<)
+                    _buildControlIcon(
+                      Icons.chevron_left,
+                      isFirstPage ? null : goToPrevPage,
+                    ),
+
+                    // 4. Go to Next Page (>)
+                    _buildControlIcon(
+                      Icons.chevron_right,
+                      isLastPage ? null : goToNextPage,
+                    ),
+
+                    // 5. Go to Last Page (>>)
+                    _buildControlIcon(
+                      Icons.last_page,
+                      isLastPage ? null : goToLastPage,
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
+}
+
+Widget _buildControlIcon(IconData icon, VoidCallback? onPressed) {
+  return IconButton(
+    icon: Icon(icon, color: Colors.white70, size: 16),
+    onPressed: onPressed,
+    // Add padding or a Material to get the hover/splash effect
+    // padding: EdgeInsets.symmetric(horizontal: 4),
+    splashRadius: 20,
+  );
 }
 
 class MyDataSource extends DataTableSource {
